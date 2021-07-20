3f0ac3fc784d1aa3a6ae9021e68fb520411d8221
Cornel Raiu <cornel.raiu@master-webtools.com>
2021-07-20 12:37:37 +0300

    add links to support page and quick rate tool

--- admin/includes/class.dashboard.php
+++ admin/includes/class.dashboard.php
@@ -66,7 +66,7 @@ if ( ! class_exists( 'MWTSA_Dashboard' ) ) {
         }
 
         public function add_plugin_meta_links($meta_fields, $file){
-            if($file == 'search-analytics/mwt-search-analytics.php') {
+            if ( $file == 'search-analytics/mwt-search-analytics.php' ) {
 
                 $meta_fields[] = "<a href='" . MWTSA_WORDPRESS_URL . "' target='_blank'>" . esc_html__( 'Support Forum', 'search-analytics' ) . "</a>";
                 $svg = "<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-star'><polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2'/></svg>";
