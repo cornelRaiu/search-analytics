65d8675605d615a5b8e18cec8b5a44a82c3287ea
Cornel Raiu <cornel.raiu@master-webtools.com>
2022-08-17 16:11:34 +0300

    more security improvements on the `run_terms_history_data_query` method + add `mwtsa_display_latest_searches` shortcode + add more filters: mwtsa_run_terms_history_order_by, mwtsa_display_latest_searches_shortcode_list_item_output, mwtsa_display_latest_searches_shortcode_output,

---
+++ shortcodes/class.mwtsa_display_latest_searches.php
@@ -0,0 +1,75 @@
+<?php
+defined("ABSPATH") || exit;
+
+if ( ! class_exists( 'MWTSA_Display_Latest_Searches_Shortcode' ) ) {
+    class MWTSA_Display_Latest_Searches_Shortcode {
+
+        public static function init() {
+            add_shortcode( 'mwtsa_display_latest_searches', array( __CLASS__, 'render' ) );
+        }
+
+	    public static function order_by_latest() {
+		    return "`datetime` DESC";
+        }
+
+        public static function render( $atts ) {
+	        $atts = shortcode_atts( array(
+		        'unit'                            => 'week',
+		        'amount'                          => 1,
+		        'count'                           => 10,
+		        'only_with_results'               => true,
+		        'wrapper_class'                   => 'mwtsa-latest-searches',
+                'label'                           =>  ''
+	        ), $atts, 'mwtsa_display_latest_searches' );
+
+	        $atts['only_with_results'] = filter_var( $atts['only_with_results'], FILTER_VALIDATE_BOOLEAN );
+
+	        if ( $atts['label'] === '' ) {
+		        $atts['label'] = ( $atts['only_with_results'] ) ?
+			        __( 'Recently Searched Terms With Results', 'search-analytics' ) :
+			        __( 'Recently Searched Terms', 'search-analytics' );
+	        }
+
+	        $search_args = array(
+		        'since' => (int) $atts['amount'],
+		        'unit'  => $atts['unit'],
+		        'group' => 'no_group',
+		        'count' => $atts['count']
+	        );
+
+	        if ( $atts['only_with_results'] ) {
+		        $search_args['min_results'] = 1;
+	        }
+
+
+	        add_filter( 'mwtsa_run_terms_history_order_by', array( __CLASS__, 'order_by_latest' ) );
+
+	        $search_results = ( new MWTSA_History_Data )->run_terms_history_data_query( $search_args );
+
+	        remove_filter( 'mwtsa_run_terms_history_order_by', array( __CLASS__, 'order_by_latest' ) );
+
+            ob_start();
+            ?>
+            <div class="<?php echo $atts['wrapper_class'] ?>">
+                <?php if ( count( $search_results ) > 0 ) : ?>
+                    <p><?php echo $atts['label'] ?></p>
+                    <ul>
+                        <?php foreach ( $search_results as $term ) :
+                            echo apply_filters(
+                                'mwtsa_display_latest_searches_shortcode_list_item_output',
+                                sprintf(
+                                    '<li>%s</li>',
+                                    esc_attr( $term['term'] )
+                                ),
+                                $atts
+                            );
+                        endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+            <?php
+
+            return apply_filters( 'mwtsa_display_latest_searches_shortcode_output', ob_get_clean(), $atts, $search_results );
+        }
+    }
+}
